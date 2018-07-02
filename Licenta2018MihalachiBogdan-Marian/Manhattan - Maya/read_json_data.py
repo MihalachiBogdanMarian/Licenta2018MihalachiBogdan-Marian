a1eae3095d8a128063323b17e327b27eee0c80fe
MihalachiBogdanMarian <b_mihalachi@yahoo.com>
2018-07-02 09:37:38 +0300

    final

--- Licenta2018MihalachiBogdan-Marian/Manhattan - Maya/read_json_data.py
+++
@@ -1,7 +0,0 @@
-import json
-
-data = open(r"C:\Users\Bidi\eclipse-workspace\Manhattan\manhattan_results.json", "rt").read()
-d = json.loads(data)
-planeDimension = d["planeDimension"]
-longestPath = d["longestPath"]
-curves = d["curves"]
